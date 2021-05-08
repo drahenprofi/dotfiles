635d7fa7f3e55a5629229eb8e000a9093db2fc52
arndtphillip <arndt.philipp@gmx.de>
2021-05-08 22:40:16 +0200

    Improved tasklist selected indicator

--- .config/awesome/widgets/tasklist/app-widget.lua
+++ .config/awesome/widgets/tasklist/app-widget.lua
@@ -19,20 +19,24 @@ local get_app_icon = function(is_template)
 
     return {
         {
-            nil,
             {
-                bg = beautiful.misc1,
-                id = "selected_indicator",
-                shape = function(cr, width, height)
-                    gears.shape.partially_rounded_rect(cr, width, height, false, true, true, false, dpi(50))
-                end,
-                forced_height = dpi(4),
-                forced_width = dpi(4),
-                widget = wibox.container.background
+                nil,
+                {
+                    bg = beautiful.misc1,
+                    id = "selected_indicator",
+                    shape = function(cr, width, height)
+                        gears.shape.rounded_rect(cr, width, height, dpi(50))
+                    end,
+                    forced_height = dpi(4),
+                    forced_width = dpi(4),
+                    widget = wibox.container.background
+                },
+                nil,
+                expand = "none",
+                widget = wibox.layout.align.vertical
             },
-            nil,
-            expand = "none",
-            widget = wibox.layout.align.vertical
+            left = dpi(3),
+            widget = wibox.container.margin
         },
         {
             {
